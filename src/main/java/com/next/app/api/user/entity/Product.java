d4b1141265c68d33a2a0a54b51e5e1db9b51f178
Mminzy22 <aw7022@gmail.com>
2025-08-01 14:31:04 +0900

    feat: Product 엔티티에 기본 생성자 및 모든 필드를 포함하는 어노테이션 추가

--- src/main/java/com/next/app/api/user/entity/Product.java
+++ src/main/java/com/next/app/api/user/entity/Product.java
@@ -1,13 +1,15 @@
 package com.next.app.api.user.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "product_minji")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
@@ -17,12 +19,4 @@ public class Product {
     private String name;
     private BigDecimal price;
     private String description;
-
-    public Product() {}
-
-    public Product(String name, BigDecimal price, String description) {
-        this.name = name;
-        this.price = price;
-        this.description = description;
-    }
 }
